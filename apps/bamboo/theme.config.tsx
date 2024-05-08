e7f7732b2c4f2f385387a5f6c06697aeb173c957
wdyjwdy <xxccat@gmail.com>
2024-05-09 03:39:56 +0800

    init

---
+++ apps/bamboo/theme.config.tsx
@@ -0,0 +1,43 @@
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  head: (
+    <>
+      <link rel='icon' href='/site/logo.png'/>
+    </>
+  ),
+  logo: (
+    <>
+      <img src='/site/logo.png' width={40} height={40} alt='logo'/>
+      <span style={{fontWeight: 800, paddingLeft: '10px'}}>Code Umbrella</span>
+    </>
+  ),
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s'
+    }
+  },
+  primaryHue: 340,
+  search: {
+    placeholder: 'Search'
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+  },
+  toc: {
+    title: null
+  },
+  editLink: {
+    component: null
+  },
+  feedback: {
+    content: null
+  },
+  navigation: false,
+  footer: {
+    component: <></>
+  },
+  darkMode: false,
+}
+
+export default config
