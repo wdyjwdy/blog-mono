e65c2fb9914b2ae368d955428dcc1ce56867f947
wdyjwdy <xxccat@gmail.com>
2024-05-11 22:57:21 +0800

    update

---
+++ apps/anchor/theme.config.tsx
@@ -0,0 +1,44 @@
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  head: (
+    <>
+      <link rel='icon' href='/site/logo.png'/>
+    </>
+  ),
+  logo: (
+    <>
+      <img src='/site/logo.png' width={40} height={40} alt='logo'/>
+      <span style={{fontWeight: 800, paddingLeft: '10px'}}>Code Anchor</span>
+    </>
+  ),
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s'
+    }
+  },
+  primaryHue: 210,
+  primarySaturation: 35,
+  search: {
+    placeholder: 'Search'
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+  },
+  toc: {
+    title: null
+  },
+  editLink: {
+    component: null
+  },
+  feedback: {
+    content: null
+  },
+  navigation: false,
+  footer: {
+    component: <></>
+  },
+  darkMode: false,
+}
+
+export default config
