18085bdbf19ed2414025eb5293b25cb6c91937c1
wdyjwdy <xxccat@gmail.com>
2024-07-26 06:00:16 +0800

    doc: data structure

--- apps/blog/components/CreateTable.tsx
+++ apps/blog/components/CreateTable.tsx
@@ -1,7 +1,7 @@
 import { Table, Flex, Button, Separator } from '@radix-ui/themes';
 import Link from 'next/link'
 
-export default function CreateTable({ data, doc, path }) {
+export default function CreateTable({ data, path, singlePage }) {
     return (
         <>
             <Table.Root variant="surface">
@@ -22,7 +22,18 @@ export default function CreateTable({ data, doc, path }) {
                             <Flex gap="1" align="center" wrap="wrap">
                               {
                                 values.flatMap((value, index) => {
-                                  let arr = value.map(x => <Link href={`/${doc}/${path}/${key.toLowerCase()}#${x.toLowerCase()}`}><Button variant="soft" size="1">{x}</Button></Link>)
+                                  let arr = value.map(x => {
+                                    let href = singlePage
+                                      ? `/${path}#${x.toLowerCase()}`
+                                      : `/${path}/${key.toLowerCase()}#${x.toLowerCase()}`
+                                    return (
+                                      <Link href={href}>
+                                        <Button variant="soft" size="1">
+                                          {x}
+                                        </Button>
+                                      </Link>
+                                    )
+                                  })
                                   arr.push(<Separator orientation="vertical" size="1" style={{ margin: "0 6px" }} />)
                                   if (index === values.length - 1) arr.pop()
                                   return arr
